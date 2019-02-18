eae6b69f23209e1e4f9f72b568790e7346972b10
markzyh <980511383@qq.com>
2019-02-18 16:15:54 +0800

    begin

---
+++ src/js/ajax/ajax.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://free-api.heweather.net/s6/weather'
+axios.defaults.headers = {
+  'Content-Type': 'application/x-www-form-urlencoded'
+}
+
+// 添加请求 拦截器
+axios.interceptors.request.use(config => {
+  //console.log(config.url)search
+  // 在发送请求之前做些什么
+  if(config.url.indexOf('location') != -1 || config.url.indexOf('search') != -1){
+    let key = `e82fb3f88fdf41898b945fda077cffbc`
+    config.url += `&key=${key}`
+    return config;
+  }else{
+    return config
+  }
+  //config.url = `now?location=${area},${city}&key=${key}`
+  
+}, error => {
+  // 对请求错误做些什么
+
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+axios.interceptors.response.use(res => {
+  // 对响应数据做点什么
+  return res
+
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
+
+
+export default axios
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,24 +1,6 @@
 <template>
-  <div class="container" @click="clickHandle('test click', $event)">
-
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
-    </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
+  <div class="index">
+hahahah
   </div>
 </template>
 
@@ -28,7 +10,7 @@ import card from '@/components/card'
 export default {
   data () {
     return {
-      motto: 'Hello World',
+      motto: 'Hello World', 
       userInfo: {}
     }
   },
@@ -39,8 +21,7 @@ export default {
 
   methods: {
     bindViewTap () {
-      const url = '../logs/main'
-      wx.navigateTo({ url })
+      
     },
     getUserInfo () {
       // 调用登录接口
