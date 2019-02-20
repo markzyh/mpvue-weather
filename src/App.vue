691df5b349de4061960aa31f3b6b04f2c7f68849
markzyh <980511383@qq.com>
2019-02-20 17:13:44 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <script>
+/* 这部分相当于原生小程序的 app.js */
 export default {
+  
   created () {
-    // 调用API从本地缓存中获取数据
+    /* // 调用API从本地缓存中获取数据
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    console.log('app created and cache logs by setStorageSync')
+    console.log('app created and cache logs by setStorageSync') */
   }
 }
 </script>
 
+
 <style>
+/* 这部分相当于原生小程序的 app.wxss */
 .container {
   height: 100%;
   display: flex;
@@ -27,5 +31,162 @@ export default {
   -moz-transition: width 2s;
   -webkit-transition: width 2s;
   -o-transition: width 2s;
+}
+/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section {
+  display: block;
+}
+body {
+  line-height: 1;
+}
+page{
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  width: 100%;
+  height: 1334px;
+  /* background-image: url(./assets/weather_bg.jpg); */
+  background-color: #3a8bcc;
+  border-top: 1px solid #3a8bcc;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+}
+ol,
+ul {
+  list-style: none;
+}
+blockquote,
+q {
+  quotes: none;
+}
+blockquote:before,
+blockquote:after,
+q:before,
+q:after {
+  content: "";
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+body{
+  font-size: 20px;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  width: 100%;
+  height: 1334px;
+  /* background-image: url(./assets/weather_bg.jpg); */
+  background-color: #3a8bcc;
+  border-top: 1px solid #3a8bcc;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
 }
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="index">
-hahahah
+    <h1 class="today">{{today}}</h1>
+    <router-link to="/region" tag="h2" class="local_title">{{city}}市-{{area}}区</router-link>
+    <h3 class="weather_status">
+      {{nowweatherStatus}}
+      <span>{{nowWindDir}}</span>
+      <span>{{nowWindSc}}级</span>
+    </h3>
+    <h4 class="now_tmp">{{nowTmp}}℃</h4>
   </div>
 </template>
 
 <script>
-import card from '@/components/card'
+import card from "@/components/card";
 
 export default {
-  data () {
+  data() {
     return {
-      motto: 'Hello World', 
-      userInfo: {}
-    }
+      c: "", //画布实例
+      diff: "", //温差
+      HEIGHT: "", //画布高度
+      ONE_HEIGHT: "", //每一度的高度,等于总高度除以温差
+      WIDTH: "", //每一天的间隔
+      RADIUS: 2, //点的半径
+      PADDING: "", //间隔,等于宽度的一半
+      max: "",
+      today: "", //当天
+      city: "上海", //本地市
+      area: "宝山", //本地区
+      day: "", //当天星期几的索引
+      nowweather: [], //当前天气信息
+      forecastWeather: [], //未来7天信息
+      nowweatherStatus: "",
+      nowWindDir: "", //风向
+      nowWindSc: "", //风力
+      nowTmp: "", //温度
+      updateTime: "", //更新时间
+      highTemp: [],
+      lowTemp: [],
+      week: [
+        "星期日",
+        "星期一",
+        "星期二",
+        "星期三",
+        "星期四",
+        "星期五",
+        "星期六"
+      ],
+      swiperOption: {
+        slidesPerView: 4
+      }
+    };
   },
 
   components: {
@@ -20,67 +58,192 @@ export default {
   },
 
   methods: {
-    bindViewTap () {
+    /* bindViewTap () {
       
+    }, */
+    //未来天气
+    handleForecastWeather() {
+      let length = this.forecastWeather.length;
+      for (let i = 0; i < length; i++) {
+        this.highTemp.push(this.forecastWeather[i].tmp_max);
+        this.lowTemp.push(this.forecastWeather[i].tmp_min);
+      }
+      this.calwidth(); //计算宽度,画布尺寸
+      this.canvas(this.highTemp, "#fcc370", this.c);
+      //this.canvas(this.highTemp, "#fcc370", this.c);
+      this.canvas(this.lowTemp, "#137bcf", this.c);
     },
-    getUserInfo () {
+    //当前天气
+    handleNowWeather() {
+      if (this.nowweather.length != 0) {
+        let nowweather = this.nowweather;
+        this.nowweatherStatus = nowweather.now.cond_txt;
+        this.nowWindDir = nowweather.now.wind_dir;
+        this.nowWindSc = nowweather.now.wind_sc;
+        this.nowTmp = nowweather.now.tmp;
+        this.updateTime = nowweather.update.loc;
+      }
+    },
+    //未来7天天气
+    getForecastWeather(area, city) {
+      let url = `forecast?location=${area},${city}`;
+      this.$axios.get(url).then(res => {
+        this.forecastWeather = res.data.HeWeather6[0].daily_forecast;
+        this.handleForecastWeather();
+        console.log(this.forecastWeather);
+      });
+    },
+    //当前天气
+    getNowWeather(area, city) {
+      let url = `now?location=${area},${city}`;
+      this.$axios.get(url).then(res => {
+        this.nowweather = res.data.HeWeather6[0];
+        console.log(this.nowweather)
+        //this.handleNowWeather();
+      });
+    },
+    //未来显示的星期
+    calWeek(index) {
+      let length = this.week.length; //7
+      if (index > length - 1) {
+        index = index - length;
+      }
+      return this.week[index];
+    },
+    getToday() {
+      let now = new Date();
+      let year = now.getFullYear();
+      let month = this.checkTen(now.getMonth() + 1);
+      let date = this.checkTen(now.getDate());
+      this.day = now.getDay();
+      let day = this.week[now.getDay()];
+      this.today = `${year}-${month}-${date} ${day}`;
+    },
+    checkTen(num) {
+      if (num > 10) {
+        return num;
+      } else {
+        return "0" + num;
+      }
+    },
+    getUserInfo() {
       // 调用登录接口
       wx.login({
         success: () => {
           wx.getUserInfo({
-            success: (res) => {
-              this.userInfo = res.userInfo
+            success: res => {
+              this.userInfo = res.userInfo;
             }
-          })
+          });
         }
-      })
+      });
     },
-    clickHandle (msg, ev) {
-      console.log('clickHandle:', msg, ev)
+    clickHandle(msg, ev) {
+      console.log("clickHandle:", msg, ev);
     }
   },
 
-  created () {
+  created() {
     // 调用应用实例的方法获取全局数据
-    this.getUserInfo()
+    this.getUserInfo();
+  },
+  mounted() {
+   /*  this.getToday(); */
+    //this.getNowWeather(this.area, this.city);
+    /* this.getForecastWeather(this.area, this.city); */
   }
-}
+};
 </script>
 
-<style scoped>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+<style lang='scss'>
+
+.today{
+  margin-top: 20px;
 }
 
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
+.project_title {
+  text-align: center;
+  font-size: 36px;
+  margin: 20px 0;
+  color: #fff;
+  text-transform: uppercase;
 }
-
-.userinfo-nickname {
-  color: #aaa;
+.tem_canvas {
+  //width: 1312.5px;
+  //height: 200px;
+  position: absolute;
+  //border:1px solid #000;
+  top: 230px;
 }
-
-.usermotto {
-  margin-top: 150px;
+.weather_details {
+  color: #333;
+  h3,
+  h4 {
+    font-size: 26px;
+    font-weight: normal;
+    margin-top: 20px;
+  }
+  p {
+    font-size: 20px;
+    margin-top: 20px;
+  }
+  .night_status {
+    //margin-top: 350px;
+  }
+  .day_status {
+    margin-top: 40px;
+  }
+  .night_status {
+    margin-bottom: 40px;
+  }
+  .empty {
+    height: 300px;
+    width: 100%;
+    //border:1px solid #ff0000
+  }
 }
-
-.form-control {
+.cond_img {
+  width: 60px;
+  height: 60px;
   display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
+  margin: 0 auto;
 }
-
-.counter {
+.forecast_title {
+  font-size: 40px;
+  text-align: center;
+  margin: 60px;
+}
+.forecast_list {
+  display: flex;
+  background: rgba(255, 255, 255, 0.5);
+  padding: 30px 0;
+  position: relative;
+}
+.now_tmp {
+  font-size: 60px;
+  margin: 20px 0;
+}
+.update_time {
+  font-size: 12px;
+  font-weight: normal;
+  display: block;
+}
+.index {
+  color: #fff;
+}
+.local_title {
+  text-align: center;
+  font-size: 36px;
+  margin: 40px 0;
+  color: #fff;
+  text-transform: uppercase;
+  border-bottom: 1px solid;
   display: inline-block;
-  margin: 10px auto;
-  padding: 5px 10px;
-  color: blue;
-  border: 1px solid blue;
+}
+.weather_status {
+  font-size: 30px;
+  span {
+    margin: 0 0 0 30px;
+  }
 }
 </style>
